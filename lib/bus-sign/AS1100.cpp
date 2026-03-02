1bc3f6b9cc00d1a15b94c67f8dd4e6f5565b742e
Adam Kuczynski <github@typedef.io>
2026-03-02 23:26:53 +0000

    Loicense and cleanup

--- lib/bus-sign/AS1100.cpp
+++ lib/bus-sign/AS1100.cpp
@@ -1,31 +1,31 @@
-/** \mainpage Hull Bus Terminal Signs
+/**
+* MIT License
  *
- * \section intro_sec Introduction
+ * Copyright (c) 2026 Adam Kuczyński
  *
- * Documentation for the code to use the Hull bus terminal signs.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The most useful page to look at is the [Panel class](./classPanel.html).
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * For hardware setup instructions see [README.md](https://github.com/ConnectedHumber/Bus-Terminal-Signs/tree/master/README.md) and [Setup.md](https://github.com/ConnectedHumber/Bus-Terminal-Signs/tree/master/Setup.md).
- *
- * For example code see [examples](https://github.com/ConnectedHumber/Bus-Terminal-Signs/tree/master/Code/Examples).
- *
- * \section contribs Contributors
- *
- * Paul Richards ee@sosgez.co.uk
- *
- * alifeee - alifeee.web@outlook.com
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include <AS1100.h>
 #include <Arduino.h>
 #include <SPI.h>
 #include <Adafruit_GFX.h>
-
-#define ROWS_PER_CHIP 8
-#define COLS_PER_CHIP 6
-#define LOAD_PULSE_WIDTH 2 // 3 microseconds, duration of MARK
-#define NEXT_PULSE_DELAY 1 // 1 microseconds, duration of SPACE following MARK
 
 AS1100::AS1100(int sck, int mosi, int cs) : GFXcanvas1(192, 9),
                                             _spi(-1, sck, -1, mosi, 100000, SPI_BITORDER_MSBFIRST, SPI_MODE3),
@@ -40,7 +40,7 @@ void AS1100::begin() {
   sendCmd(0x0E00 + (2 & 3)); // set clock to internal
   sendCmd(0x0E00 + (0 & 3)); // set clock to external
   sendCmd(0x0900); // set binary mode
-  sendCmd(0x0B00 + ((ROWS_PER_CHIP - 1) & 7)); // 7 means 8 blocks of 8 pixels
+  sendCmd(0x0B07); // 7 means 8 blocks of 8 pixels
 
   setIntensity(2, -1); // start low.
   sendCmd(0x0C01); // 0x0C01 - display on, 0x0C00 - display off
@@ -49,11 +49,8 @@ void AS1100::begin() {
 }
 
 void AS1100::load() {
-  // load pulse causes data to be loaded and displayed if display is on
-  digitalWrite(_loadPin, LOW); // transfer from shift register to display drivers buffer
-  delayMicroseconds(LOAD_PULSE_WIDTH);
+  digitalWrite(_loadPin, LOW);
   digitalWrite(_loadPin, HIGH);
-  delayMicroseconds(NEXT_PULSE_DELAY);
 }
 
 void AS1100::writeDigit(int digit, uint8_t d) {
